701df9f470679202e552f2dc8d64b9ffd3d9e8ee
Paolo Raez <str237@gmail.com>
2022-02-02 23:00:12 +0100

    Define client

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::io::{stdin, BufRead};
 
+struct Client {
+  likes: Vec<usize>,
+  dislikes: Vec<usize>
+}
+
 fn init() {
   let stdin = stdin();
   let mut line_iter = stdin.lock().lines();
   let first: String = line_iter.next().unwrap().unwrap();
   let n: usize = first.parse().unwrap();
-  for i in 0..n {
+  for _i in 0..n {
 
   }
 }
