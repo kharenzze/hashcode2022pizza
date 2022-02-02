e10be7691817090e1cf7e8051a90d2b2399f4d24
Paolo Raez <str237@gmail.com>
2022-02-02 23:33:31 +0100

    WIP ingest line

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
+use std::collections::{HashMap, HashSet};
 use std::io::{stdin, BufRead};
 
+type TokenSet = HashSet<usize>;
 struct Client {
-  likes: Vec<usize>,
-  dislikes: Vec<usize>
+  likes: TokenSet,
+  dislikes: TokenSet,
+}
+
+struct Game {
+  clients: Vec<Client>,
+  tokens: HashMap<String, usize>,
+}
+
+impl Game {
+  fn ingest_line(&mut self, l: &str) -> TokenSet {
+    let mut ingredient_iter = l.split_ascii_whitespace();
+    let n: usize = ingredient_iter.next().unwrap().parse().unwrap();
+    if n == 0 {
+      return TokenSet::new();
+    }
+    let mut tokens = TokenSet::with_capacity(n);
+    for _i in 0..n {
+      let ing: &str = ingredient_iter.next().unwrap();
+      let token = self.get_token(ing);
+      tokens.insert(token);
+    }
+    tokens
+  }
+
+  pub fn get_token(&self, t: &str) -> usize {
+    1
+  }
 }
 
 fn init() {
@@ -10,9 +38,7 @@ fn init() {
   let mut line_iter = stdin.lock().lines();
   let first: String = line_iter.next().unwrap().unwrap();
   let n: usize = first.parse().unwrap();
-  for _i in 0..n {
-
-  }
+  for _i in 0..n {}
 }
 
 fn main() {
