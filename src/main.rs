f69f884f70255ec32f8e85c5331ff13cdfe30a02
Paolo Raez <str237@gmail.com>
2022-02-02 22:56:32 +0100

    Main parsing loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{stdin, BufRead};
+
+fn init() {
+  let stdin = stdin();
+  let mut line_iter = stdin.lock().lines();
+  let first: String = line_iter.next().unwrap().unwrap();
+  let n: usize = first.parse().unwrap();
+  for i in 0..n {
+
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+  init()
 }
