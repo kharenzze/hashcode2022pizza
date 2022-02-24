64333d343a332fbe33e582621350c80c8caf1e31
davidivad96 <david@strivelabs.io>
2022-02-24 19:28:19 +0100

    Read input

--- src/main.rs
+++ src/main.rs
@@ -51,155 +51,61 @@ struct Game {
 }
 
 impl Game {
-  fn ingest_line(&mut self, l: &str, like: bool) -> IngredientSet {
-    let mut ingredient_iter = l.split_ascii_whitespace();
-    let n: usize = ingredient_iter.next().unwrap().parse().unwrap();
-    if n == 0 {
-      return IngredientSet::default();
-    }
-    let info = IngredientSet {
-      set: TokenSet::with_capacity(n),
-      hash: String::new(),
-    };
-    let sorted: BinaryHeap<usize> = ingredient_iter
-      .map(|ing| {
-        let t = self.get_or_token(ing);
-        let count = match like {
-          true => &mut self.simple_count.likes,
-          false => &mut self.simple_count.dislikes,
-        };
-        let &current = count.get(&t).unwrap_or(&0);
-        count.insert(t, current + 1);
-        t
-      })
-      .collect();
-    let mut info: IngredientSet = sorted.into_iter().fold(info, |mut acc, token| {
-      acc.set.insert(token);
-      acc.hash.push_str(&format!("{}-", token));
-      acc
-    });
-    info.hash.pop();
-    info
-  }
-
-  fn get_or_token(&mut self, t: &str) -> usize {
-    let cached = self.tokens.get(t);
-    if let Some(&c) = cached {
-      return c;
-    }
-    let next_value = self.tokens.len() + 1;
-    self.tokens.insert(t.to_string(), next_value);
-    next_value
-  }
-
-  fn insert_client(&mut self, client: Client) {
-    self.clients.push(client);
-  }
-
-  fn get_solution_string(&self, tokens: &TokenSet) -> String {
-    let mut solution = format!("{} ", tokens.len());
-    for t in tokens {
-      //let pair = self.tokens.iter().find(|&(_, value)| t == value);
-      let ingredient = self.reversed_tokens.get(t).expect("token must exist");
-      solution.push_str(ingredient);
-      solution.push_str(" ");
-    }
-    solution.pop();
-    solution
-  }
-
-  fn simple_solution(&self) -> TokenSet {
-    self
-      .simple_count
-      .likes
-      .iter()
-      .map(|(&key, &value)| {
-        let &dislike_count = self.simple_count.dislikes.get(&key).unwrap_or(&0);
-        (key, value as i32 - dislike_count as i32)
-      })
-      .filter(|&(_, value)| value >= 0)
-      .map(|(key, _)| key)
-      .collect()
-  }
-
-  fn advanced_solution(&self) -> TokenSet {
-    let simple = self.simple_solution();
-    let mut dislike_options: BinaryHeap<(usize, usize)> = simple
-      .iter()
-      .map(|&token| {
-        (
-          self
-            .simple_count
-            .dislikes
-            .get(&token)
-            .unwrap_or(&0)
-            .to_owned(),
-          token,
-        )
-      })
-      .collect();
-    let mut solution: TokenSet = simple.clone();
-    let mut best_score = self.measure(&solution);
-
-    while let Some((_, token)) = dislike_options.pop() {
-      solution.remove(&token);
-      let score = self.measure(&solution);
-      if score > best_score {
-        best_score = score;
-      } else {
-        solution.insert(token);
-      }
-    }
-
-    let all: TokenSet = self.tokens.values().cloned().collect();
-
-    let rest: TokenSet = all.difference(&solution).cloned().collect();
-
-    for token in rest {
-      solution.insert(token);
-      let score = self.measure(&solution);
-      if score > best_score {
-        best_score = score;
-      } else {
-        solution.remove(&token);
-      }
-    }
-
-    solution
-  }
-
   fn init(&mut self, filename: &str) {
     let file: File = File::open(filename).expect(&format!("Cannot open file {}", filename));
     let reader = BufReader::new(file);
     let mut line_iter = reader.lines();
-    let first: String = line_iter.next().unwrap().unwrap();
-    let n: usize = first.parse().unwrap();
-    for _i in 0..n {
-      let line: String = line_iter.next().unwrap().unwrap();
-      let likes = self.ingest_line(&line, true);
-      let line: String = line_iter.next().unwrap().unwrap();
-      let dislikes = self.ingest_line(&line, false);
-      let client = Client { likes, dislikes };
-      self.insert_client(client);
+    let mut get_line = || line_iter.next().unwrap().unwrap();
+    let first: String = get_line();
+    let first_vec = first.split(" ").collect::<Vec<&str>>();
+    let C = first_vec[0].parse::<usize>().unwrap();
+    let P = first_vec[1].parse::<usize>().unwrap();
+    for _i in 0..C {
+      let contributor: String = get_line();
+      let contributor_vec = contributor.split(" ").collect::<Vec<&str>>();
+      let contributor_name: String = contributor_vec[0].to_string();
+      let n_skills: usize = contributor_vec[1].parse::<usize>().unwrap();
+      for _j in 0..n_skills {
+        let skill: String = get_line();
+        let skill_vec = skill.split(" ").collect::<Vec<&str>>();
+        let skill_name: String = skill_vec[0].to_string();
+        let skill_level: usize = skill_vec[1].parse::<usize>().unwrap();
+        self
+          .contributors
+          .get_mut(&contributor_name)
+          .unwrap()
+          .skills
+          .insert(
+            skill_name.clone(),
+            Skill {
+              name: skill_name.clone(),
+              level: skill_level,
+            },
+          );
+      }
     }
-
-    self.reversed_tokens = self.tokens.iter().fold(
-      ReversedTokenMap::with_capacity(self.tokens.len()),
-      |mut acc, (key, &value)| {
-        acc.insert(value, key.clone());
-        acc
-      },
-    )
-  }
-
-  fn measure(&self, tokens: &TokenSet) -> usize {
-    self
-      .clients
-      .iter()
-      .filter(|c| {
-        tokens.is_superset(&c.likes.set) && tokens.intersection(&c.dislikes.set).count() == 0
-      })
-      .count()
+    for _i in 0..P {
+      let project: String = get_line();
+      let project_vec = project.split(" ").collect::<Vec<&str>>();
+      let project_name: String = project_vec[0].to_string();
+      let project_days: usize = project_vec[1].parse::<usize>().unwrap();
+      let project_score: usize = project_vec[2].parse::<usize>().unwrap();
+      let project_best_before: usize = project_vec[3].parse::<usize>().unwrap();
+      let project_n_contributors: usize = project_vec[4].parse::<usize>().unwrap();
+      for _j in 0..project_n_contributors {
+        let contributor: String = get_line();
+        let contributor_vec = contributor.split(" ").collect::<Vec<&str>>();
+        let contributor_name: String = contributor_vec[0].to_string();
+        let level: usize = contributor_vec[1].parse::<usize>().unwrap();
+        self.projects.get_mut(&project_name).unwrap().skills.insert(
+          contributor_name.clone(),
+          Skill {
+            name: contributor_name.clone(),
+            level: level,
+          },
+        );
+      }
+    }
   }
 }
 
@@ -213,26 +119,6 @@ fn main() {
   let mut timer = LocalTimer::new();
   game.init(filename);
   timer.step("Init");
-
-  game.simple_solution();
-  timer.step("Tokens");
-  let tokens = game.advanced_solution();
-  timer.step("Tokens Advanced");
-  let solution = game.get_solution_string(&tokens);
-  timer.step("toString");
-
-  println!("Points: {}", game.measure(&tokens));
-  timer.step("Measure");
-
-  let result_filename = format!("{}.result", filename);
-  let mut file = OpenOptions::new()
-    .write(true)
-    .create(true)
-    .truncate(true)
-    .open(&result_filename)
-    .expect("Cannot open file");
-
-  file
-    .write_all(solution.as_bytes())
-    .expect("Cannot write file");
+  println!("{:?}", game);
+  println!("{:?}", game);
 }
